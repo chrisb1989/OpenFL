5657d114e4e5ede14e24d56a7e1630194979f02a
lavachemist <48248311+lavachemist@users.noreply.github.com>
2020-05-19 14:12:18 -0400

    Add files via upload

--- Community-PythonScripts/Scripts/ZOffset_V2.py
+++ Community-PythonScripts/Scripts/ZOffset_V2.py
@@ -22,17 +22,25 @@ if dev is None:
 
 dev.set_configuration()
 
+#This section sets the read and write parameters for USB communication, according to the settings above
+
 def writeUSB(data):
 	return dev.write(RX_EP, data, timeout=None)
 def readUSB(buffsize=1024):
 	return dev.read(TX_EP, buffsize, timeout=10000)
 
 
+'''	This loop asks the user for a Z Offset value, then it updates the user.ini file on the Form1+ SD card via USB.
+	If you press Enter at the prompt without giving an offset value, your ZOffset is unchanged.
+	If you give the script a 
+'''
+
 x = 0
 while x == 0:
         ZOffset = raw_input("Enter Z Offset OR Press Enter to Skip:")
         try:
                 if ZOffset != "":
+                	float(ZOffset) # makes sure you provided a number.
                 	inText = bytearray("c   /user.ini")
                 	blankSpace = bytearray(118)
                 	outText1 = bytearray("s0[serializerSettings]\n\rzOffset="+str(ZOffset)+"\n\r\n\r\n\r[userSettings]\n\rxOffset=0\n\rxScale3=1\n\ryOffset=0\n\ryScale3=1\n\r")
