d459b89bb9449d030e43dd7ac209cf66ba012196
lavachemist <48248311+lavachemist@users.noreply.github.com>
2020-08-15 13:20:27 -0400

    Update XY_CalibrationChecker_V1_081520.py

--- F1+ Hacking/XY_CalibrationChecker_V1_081520.py
+++ F1+ Hacking/XY_CalibrationChecker_V1_081520.py
@@ -1,12 +1,10 @@
 from OpenFL import Printer, FLP
 
-# use this for a real printer:
+use this for a real printer:
 p=Printer.Printer()
 
-# OR
-
-# use this for Dummy printer (comment out the start_printing command, or the script will fail):
-# p=Printer.DummyPrinter()
+# uncomment this for Dummy printer (comment out the start_printing command, or the script will fail):
+#p=Printer.DummyPrinter()
 
 # variables (don't change these)
 F=FLP
