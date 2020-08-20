2c84cffc41aec139044763cb2862dc0794c66bf2
lavachemist <thelavachemist@gmail.com>
2020-08-20 16:12:10 -0400

    Update OpenCV_CameraCalibration_082020.py

--- Community-PythonScripts/Scripts/The Grid Calibration Tool/OpenCV_CameraCalibration_082020.py
+++ Community-PythonScripts/Scripts/The Grid Calibration Tool/OpenCV_CameraCalibration_082020.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 import time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -13,18 +15,20 @@ objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-# This is the rtsp stream capture
-cap = cv2.VideoCapture("rtsp://laser:laser@192.168.86.24/live")
-
-while True:
-	ret, frame = cap.read()
-	# convert from BGR color to grayscale:
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	# show the grayscale image feed (for testing)
-	#cv2.imshow('gray', gray)
-	# # Find the chess board corners
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (1280, 720)
+camera.framerate = 30
+rawCapture = PiRGBArray(camera, size=(1280, 720))
+# allow the camera to warmup
+time.sleep(0.1)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
-	# # If found, add object points, image points (after refining them)
 	if ret == True:
 		objpoints.append(objp)
 		corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
@@ -36,21 +40,20 @@ while True:
 		#cv2.imshow('img', img)
 		#cv2.waitKey(0)
 		ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-		cv2.waitKey(0)
+		time.sleep(.1)
 		h,  w = img.shape[:2]
 		newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
 		# undistort
 		mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
 		dst = cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
-
+		rawCapture.truncate(0)
 		# crop the image
-		x,y,w,h = roi
-		dst = dst[y:y+h, x:x+w]
+		# x,y,w,h = roi
+		# dst = dst[y:y+h, x:x+w]
 		cv2.imshow('dst', dst)
-		cv2.waitKey(0)
+		time.sleep(.1)
 
 	# break the while loop if user presses 'q' key
 	if cv2.waitKey(1000) & 0xFF == ord('q'):
 		break
-cap.release()
 cv2.destroyAllWindows()
