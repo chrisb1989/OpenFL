141eb62fe34276da72101a9228151818696a0758
lavachemist <thelavachemist@gmail.com>
2020-08-20 13:36:00 -0400

    Update OpenCV_CameraCalibration_082020.py

--- Community-PythonScripts/Scripts/The Grid Calibration Tool/OpenCV_CameraCalibration_082020.py
+++ Community-PythonScripts/Scripts/The Grid Calibration Tool/OpenCV_CameraCalibration_082020.py
@@ -6,8 +6,8 @@ import time
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -33,19 +33,21 @@ while True:
 		img = cv2.drawChessboardCorners(gray, (9,6), corners2,ret)
 		# cv2.imshow('frame', frame)
 		# calibration
-		cv2.imshow('img', img)
-		cv2.waitKey(0)
-
+		#cv2.imshow('img', img)
+		#cv2.waitKey(0)
 		ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 		cv2.waitKey(0)
 		h,  w = img.shape[:2]
 		newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
 		# undistort
-		dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+		mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
+		dst = cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
+
 		# crop the image
 		x,y,w,h = roi
 		dst = dst[y:y+h, x:x+w]
 		cv2.imshow('dst', dst)
+		cv2.waitKey(0)
 
 	# break the while loop if user presses 'q' key
 	if cv2.waitKey(1000) & 0xFF == ord('q'):
