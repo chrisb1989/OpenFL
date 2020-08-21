52e8f41b0991ec740d5571bcae13a5628b89ec9d
lavachemist <thelavachemist@gmail.com>
2020-08-21 07:12:52 -0400

    Added photo capture

---
+++ Community-PythonScripts/Scripts/The Grid Calibration Tool/PiCamera_CapturePhotos_082020.py
@@ -0,0 +1,26 @@
+from imutils.video import VideoStream
+import datetime
+import argparse
+import imutils
+import time
+import cv2
+import numpy as np
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--picamera", type=int, default=-1,
+	help="whether or not the Raspberry Pi camera should be used")
+args = vars(ap.parse_args())
+# initialize the video stream and allow the cammera sensor to warmup
+vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
+time.sleep(2.0)
+
+iterator = 0
+
+while iterator < 10:
+	frame = vs.read()
+	filename = "savedImage_"+str(iterator)+".jpg"
+	cv2.imshow('frame', frame)
+	cv2.imwrite(filename, frame)
+	cv2.waitKey(0)
+	iterator += 1
