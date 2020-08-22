f2e96e42e1fea2a9ccc0cc6e2f3c919f553a1c40
lavachemist <thelavachemist@gmail.com>
2020-08-22 19:07:32 -0400

    Update PiCamera_LiveUnwarp_082020.py

--- Community-PythonScripts/Scripts/The Grid Calibration Tool/PiCamera_LiveUnwarp_082020.py
+++ Community-PythonScripts/Scripts/The Grid Calibration Tool/PiCamera_LiveUnwarp_082020.py
@@ -12,69 +12,38 @@ import yaml
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--picamera", type=int, default=-1,
 	help="whether or not the Raspberry Pi camera should be used")
-ap.add_argument("-r", "--radius", type = int,
-	help = "radius of Gaussian blur; must be odd")
 args = vars(ap.parse_args())
 # initialize the video stream and allow the cammera sensor to warmup
 vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
 time.sleep(2.0)
 
-# # termination criteria
-# criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-# # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-# objp = np.zeros((6*9,3), np.float32)
-# objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
-
-# # Arrays to store object points and image points from all the images.
-# objpoints = [] # 3d point in real world space
-# imgpoints = [] # 2d points in image plane.
-
+# import the calibration data
 camCalDataYaml = open("calibration_matrix.yaml")
 camCalData = yaml.load(camCalDataYaml, Loader=yaml.FullLoader)
 mtx = np.asarray(camCalData['camera_matrix'])
 dist = np.asarray(camCalData['dist_coeff'])
 
-
-# print("type of mtx is: "+str(type(mtx)))
-# print("dir of mtx is: "+str(dir(mtx)))
-# print("dir of dist is: "+str(dir(dist)))
-# print("type of dist is: "+str(type(dist)))
-
+# a function that finds the brightest spot and draws a circle around it
 def brightSpot():
+	# create a grey version of the stream
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	# perform a naive attempt to find the (x, y) coordinates of
-	# the area of the image with the largest intensity value
-	#(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
-	#cv2.circle(image, maxLoc, 5, (255, 0, 0), 2)
-	# display the results of the naive attempt
-	#cv2.imshow("Naive", image)
-	# apply a Gaussian blur to the image then find the brightest
-	# region
-	radius = 9
+	# apply a Gaussian blur to the grey version then find the brightest region
 	gray = cv2.GaussianBlur(gray, (5, 5, 0))
 	(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
-	cv2.circle(img, maxLoc, args["radius"], (255, 0, 0), 2)
-	# display the results of our newly improved method
-	cv2.imshow("LaserPoint", img)
+	cv2.circle(img, maxLoc, 5, (255, 0, 0), 2)
+	# display the results in a window called LaserPoint
+	# wait 1 millisecond
 	cv2.waitKey(1)
 
 
 # loop over the frames from the video stream
 while True:
 	frame = vs.read()
-	# h,  w = frame.shape[:2]
+	# unwarp the stream
 	img = cv2.undistort(frame, mtx, dist)
-	# undistort
-	# mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
-	# img = cv2.remap(frame,mapx,mapy,cv2.INTER_LINEAR)
-	# crop the image
-	#x,y,w,h = roi
-	#dst = dst[y:y+h, x:x+w]
-	#cv2.imshow("Original", frame) # show the original frame
-	#cv2.imshow('Unwarped', img) # show the fixed image
-	#cv2.waitKey(1)
+	# run the brightSpot function to find the laser point
 	brightSpot()
+	cv2.imshow("LaserPoint", img)
 	# break the while loop if user presses 'q' key
 	if cv2.waitKey(1000) & 0xFF == ord('q'):
 		break
