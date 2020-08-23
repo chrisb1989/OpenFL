c34bcd821e21d33ffccf9f48a36b278dda2e05f1
lavachemist <thelavachemist@gmail.com>
2020-08-23 19:37:36 -0400

    Update PiCamera_Combined_082020.py

--- Community-PythonScripts/Scripts/The Grid Calibration Tool/PiCamera_Combined_082020.py
+++ Community-PythonScripts/Scripts/The Grid Calibration Tool/PiCamera_Combined_082020.py
@@ -125,9 +125,9 @@ def viewStream():
 	cv2.imwrite("04_blurred.png", targetImg)
 	targetImg = cv2.Canny(targetImg, 80.0, 40.0, 3, L2gradient=True)
 	cv2.imwrite("05_canny.png", targetImg)
-	targetImg = cv2.equalizeHist(targetImg, targetImg)
+	#targetImg = cv2.equalizeHist(targetImg, targetImg)
 	#cv2.imwrite("06_equalizeHist.png", targetImg)
-	_ ,targetImg = cv2.threshold(targetImg, 200, 255, cv2.THRESH_BINARY)
+	_ ,targetImg = cv2.threshold(targetImg, 180, 255, cv2.THRESH_BINARY)
 	#cv2.imwrite("07_threshold.png", targetImg)
 	kernel = cv2.getStructuringElement(	cv2.MORPH_RECT, (3,3))
 	targetImg = cv2.morphologyEx(targetImg, cv2.MORPH_CLOSE, kernel, iterations=5)
@@ -140,7 +140,7 @@ def viewStream():
 		cv2.rectangle(im2, (retval[0], retval[1]), (retval[0]+retval[2], retval[1]+retval[3]), 100)
 		# print(retval) # for testing only remove later
 		targetImg[centerX, centerY] = 100
-	cv2.imwrite("08_contours.png", im2)
+	cv2.imwrite("08_contours.png", targetImg)
 
 	# loop over the frames from the video stream
 	while True:
